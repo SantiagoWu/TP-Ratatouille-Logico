3d0d4ccc37ebdec29a6394be5203b9b6e1865d83
SantiagoWu <49445577+SantiagoWu@users.noreply.github.com>
2019-06-25 09:42:43 -0300

    Add files via upload

---
+++ RatatouilleLogico.pl
@@ -0,0 +1,46 @@
+rata(remy, gusteaus).
+rata(emile, bar).
+rata(django, pizzeria).
+
+cocina(linguini, ratatouille, 3).
+cocina(linguini, sopa, 5). 
+cocina(colette, salmonAsado, 9).
+cocina(horst, ensaladaRusa, 8).
+
+trabajaEn(gusteaus, linguini).
+trabajaEn(gusteaus, colette).
+trabajaEn(gusteaus, skinner).
+trabajaEn(gusteaus, horst).
+trabajaEn(cafeDes2Moulins, amelie).
+
+%PUNTO 1
+inspeccionSatisfactoria(Restaurante):-
+	not(rata(_,Restaurante)).
+
+%PUNTO 2
+chef(Empleado,Restaurante):-
+	trabajaEn(Restaurante,Empleado),
+	cocina(Empleado,Plato,_).
+
+%PUNTO 3
+chefcito(Rata):-
+	rata(Rata,Restaurante),
+	Restaurante=gusteaus.
+	
+%TAMBIEN PODRIA HABER SIDO
+%chefcito(Rata):-
+%	rata(Rata,Restaurante),
+%	trabajaEn(Restaurante,linguini).
+
+%PUNTO 4
+cocinaBien(Alguien,Plato):-
+	cocina(Alguien,Plato,Experiencia),
+	Experiencia>7.
+
+cocinaBien(remy,_).
+
+%PUNTO 5
+encargadoDe(Alguien,Plato,Restaurante):-
+	cocina(Alguien,Plato,Exp),
+	trabajaEn(Restaurante,Alguien),
+	forall(cocina(AlguienMas,Plato,OtraExp),(Alguien\=AlguienMas,(trabajaEn(Restaurante,AlguienMas),Exp>OtraExp))).
