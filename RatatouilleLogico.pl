c6bdcf1f3dfc49f534931356a14ce4172ee5df3d
SantiagoWu <49445577+SantiagoWu@users.noreply.github.com>
2019-06-25 10:33:11 -0300

    Update RatatouilleLogico.pl

--- RatatouilleLogico.pl
+++ RatatouilleLogico.pl
@@ -47,3 +47,34 @@ encargadoDe(Alguien,Plato,Restaurante):-
 	forall((cocina(AlguienMas,Plato,Exp2),trabajaEn(Restaurante,AlguienMas),Alguien\=AlguienMas),Exp>Exp2).
 
 %forall(condiciones para quien, que debe cumplir).
+
+%PUNTO 6
+
+plato(ensaladaRusa, entrada([papa, zanahoria, arvejas, huevo, mayonesa])).
+plato(bifeDeChorizo, principal(pure, 20)).
+plato(frutillasConCrema, postre(265)).
+
+saludable(Plato):-
+	plato(Plato,entrada(Ingredientes)),
+	length(Ingredientes,Cant),
+	Calorias is Cant*15,
+	Calorias<75.
+
+saludable(Plato):-
+	plato(Plato,principal(pure,HdeC)),
+	Calorias is HdeC*5+20,
+	Calorias<75.
+
+saludable(Plato):-
+	plato(Plato,principal(papasFritas,HdeC)),
+	Calorias is HdeC*5+50,
+	Calorias<75.
+
+saludable(Plato):-
+	plato(Plato,principal(ensalada,HdeC)),
+	Calorias is HdeC*5,
+	Calorias<75.
+
+saludable(Plato):-
+	plato(Plato, postre(Calorias)),
+	Calorias<75.
